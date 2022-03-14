546c209646d57ad8a6b27d6e4922317c358b85fe
Angelo Bassig <angelo.bassig@pointwest.com.ph>
2022-03-14 09:51:15 +0800

    updated friendcontroller 314

--- src/main/java/com/pointwest/pastebook/pastebook_backend/controllers/FriendController.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/controllers/FriendController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @RestController
 @CrossOrigin
 public class FriendController {
@@ -13,16 +15,26 @@ public class FriendController {
     private FriendService friendService;
 
     // accepting friend
-    @RequestMapping(value="/friends/{requesterId}/{recipientId}", method = RequestMethod.POST)
-    public ResponseEntity<Object> acceptFriend(@PathVariable Long requesterId, @PathVariable Long recipientId) {
-        return friendService.acceptFriend(requesterId, recipientId);
+    @RequestMapping(value="/api/friends", method = RequestMethod.POST)
+    public ResponseEntity<Object> acceptFriend(@RequestBody HashMap<String, Object> friendMap) {
+        return friendService.acceptFriend(friendMap);
     }
 
     // deleting friend
-    @RequestMapping(value="/friends/{requesterId}/{recipientId}", method = RequestMethod.DELETE)
+    @RequestMapping(value="/api/friends/{requesterId}/{recipientId}", method = RequestMethod.DELETE)
     public ResponseEntity<Object> deleteFriend(@PathVariable Long requesterId, @PathVariable Long recipientId) {
         return friendService.deleteFriend(requesterId, recipientId);
     }
 
+    // get all friends given a particular page id
+    @RequestMapping(value="/api/friends/{pageId}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getFriends(@PathVariable Long pageId) {
+        return friendService.getFriends(pageId);
+    }
 
+    // get one friend
+    @RequestMapping(value="/api/friends/{requesterId}/{recipientId}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getOneFriend(@PathVariable Long requesterId, @PathVariable Long recipientId) {
+        return friendService.getOneFriend(requesterId, recipientId);
+    }
 }
