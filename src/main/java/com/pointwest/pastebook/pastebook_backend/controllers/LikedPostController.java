b776bafde6535073280c064a3d34ccac37997632
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-14 20:03:38 +0800

    Added getLikes endpoint

--- src/main/java/com/pointwest/pastebook/pastebook_backend/controllers/LikedPostController.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/controllers/LikedPostController.java
@@ -1,9 +1,12 @@
 package com.pointwest.pastebook.pastebook_backend.controllers;
 
+import com.pointwest.pastebook.pastebook_backend.models.User;
 import com.pointwest.pastebook.pastebook_backend.services.LikedPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -21,5 +24,10 @@ public class LikedPostController {
   public ResponseEntity<Object> unLikePost(
       @PathVariable Long postId, @RequestHeader(value = "Authorization") String stringToken) {
     return likedPostService.unlikePost(postId, stringToken);
+  }
+
+  @GetMapping("/like/{postId}")
+  public ResponseEntity<Integer> getLikes(@PathVariable Long postId) {
+    return ResponseEntity.ok().body(likedPostService.getLikes(postId));
   }
 }
