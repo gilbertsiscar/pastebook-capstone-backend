d4aedc8f873b81c94e6e1fd264145634cb6bf9b1
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-14 11:51:43 +0800

    Refactored post service

--- src/main/java/com/pointwest/pastebook/pastebook_backend/services/PostService.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/services/PostService.java
@@ -1,23 +1,12 @@
 package com.pointwest.pastebook.pastebook_backend.services;
 
 import com.pointwest.pastebook.pastebook_backend.models.Post;
-import org.springframework.http.ResponseEntity;
-
-import java.util.ArrayList;
 
 public interface PostService {
 
-    // creating a post
-    ResponseEntity createPost(Post post, String token, ArrayList<String> taggedIds);
+  Post createPost(Post post, String token);
 
-    Iterable<Post> getPostsFromUser(String stringToken);
-    Iterable<Post> getTaggedPosts(String stringToken);
-    Iterable<Post> getAllPostRelatedToUser(Long userId, String stringToken);
+  Post getPostById(Long id);
 
-
-
-    // getting all posts of a particular user
-    //ResponseEntity getPostsFromUser(Long visitorId, Long ownerId);
-
-    //ResponseEntity getPostsRelatedToUser(String token);
+  Iterable<Post> getPostsFromUser(String stringToken);
 }
