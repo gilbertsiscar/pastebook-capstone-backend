98365ee3d96a1f2f10c563bebffb0e106d9e07c1
Angelo Bassig <angelo.bassig@pointwest.com.ph>
2022-03-14 09:58:08 +0800

    updated friend service impl 314

--- src/main/java/com/pointwest/pastebook/pastebook_backend/services/FriendServiceImpl.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/services/FriendServiceImpl.java
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Service
 public class FriendServiceImpl implements FriendService {
@@ -27,8 +30,15 @@ public class FriendServiceImpl implements FriendService {
     private FriendRequestRepository friendRequestRepository;
 
     // accepting friend
-    public ResponseEntity acceptFriend(Long requesterId, Long recipientId) {
+    public ResponseEntity acceptFriend(HashMap<String, Object> friendMap) {
         // need to add condition for the case where person A and person B BOTH sent a friend request to each other, but naunang i accept ni person A si person B (but still, the friend request person B has from person A is still existing), pero we can just add a conditional na kapag in-accept ito ni person B, sasabihin ("You're already friends [with Person A])
+
+        String requesterIdStr = friendMap.get("requesterId").toString();
+        String recipientIdStr = friendMap.get("recipientId").toString();
+
+        Long requesterId = Long.parseLong(requesterIdStr);
+        Long recipientId = Long.parseLong(recipientIdStr);
+
         for (Friend friend : friendRepository.findAll()) {
             if ((friend.getRequester().getId() == requesterId && friend.getRecipient().getId() == recipientId) || (friend.getRequester().getId() == recipientId && friend.getRecipient().getId() == requesterId)) {
 
@@ -67,6 +77,13 @@ public class FriendServiceImpl implements FriendService {
                 // deleting the record with the very same requesterId and recipientId in the friend_requests table
                 friendRequestRepository.deleteById(friendRequest.getId());
 
+                // also deleting the record (if sabay na nag-add yung 2 people sa isa't isa)
+                for (FriendRequest request : friendRequestRepository.findAll()) {
+                    if (request.getSender().getId() == recipientId && request.getReceiver().getId() == requesterId) {
+                        friendRequestRepository.deleteById(request.getId());
+                    }
+                }
+
                 return new ResponseEntity("Accepted the friend request!", HttpStatus.CREATED);
             }
         }
@@ -75,6 +92,7 @@ public class FriendServiceImpl implements FriendService {
 
     // deleting friend
     public ResponseEntity deleteFriend(Long requesterId, Long recipientId) {
+
         for (Friend friend : friendRepository.findAll()) {
             if ((friend.getRequester().getId() == requesterId && friend.getRecipient().getId() == recipientId) || (friend.getRequester().getId() == recipientId && friend.getRecipient().getId() == requesterId)) {
                 friendRepository.deleteById(friend.getId());
@@ -85,4 +103,47 @@ public class FriendServiceImpl implements FriendService {
     }
 
     // VERY IMPORTANT NOTE: the friends table is actually really complex because 'friends' is bi-directional! we just added a conditional statement for all the methods here to cover this bi-directional relationship
+
+    // get all friends given a particular page id
+    public ResponseEntity getFriends(Long pageId) {
+        ArrayList<User> friendListPart1 = new ArrayList<>();
+        ArrayList<User> friendListPart2 = new ArrayList<>();
+
+        // looping through the first column in the friends table
+        for (Friend friend : friendRepository.findAll()) {
+            if (friend.getRequester().getId() == pageId) {
+                // get the corresponding friend.getRecipient and put it into an array list
+                friendListPart1.add(friend.getRecipient());
+            }
+        }
+
+        // looping through the second column in the friends table
+        for (Friend friend : friendRepository.findAll()) {
+            if (friend.getRecipient().getId() == pageId) {
+                // get the corresponding friend.getRequester and put it into an array list
+                friendListPart1.add(friend.getRequester());
+            }
+        }
+
+        // combining the two array lists together
+        friendListPart1.addAll(friendListPart2);
+
+        return new ResponseEntity(friendListPart1, HttpStatus.OK);
+
+    }
+
+    // get one friend
+    public ResponseEntity getOneFriend(Long requesterId, Long recipientId) {
+        for (Friend friend : friendRepository.findAll()) {
+            if ((requesterId == friend.getRequester().getId() && recipientId == friend.getRecipient().getId()) || (requesterId == friend.getRecipient().getId() && requesterId == friend.getRequester().getId())) {
+                Optional<Friend> optionalFriend = Optional.of(friend);
+                System.out.println(optionalFriend);
+                return new ResponseEntity(optionalFriend, HttpStatus.OK);
+            }
+        }
+        Optional<Friend> noFriend = Optional.of(new Friend());
+        return new ResponseEntity(noFriend, HttpStatus.OK);
+    }
+
+
 }
