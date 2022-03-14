88b0fcdb74db4c4d58669a99f2d6eb78b4b1d7fc
Angelo Bassig <angelo.bassig@pointwest.com.ph>
2022-03-14 09:57:04 +0800

    updated friend service 314

--- src/main/java/com/pointwest/pastebook/pastebook_backend/services/FriendService.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/services/FriendService.java
@@ -2,13 +2,21 @@ package com.pointwest.pastebook.pastebook_backend.services;
 
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
+
 public interface FriendService {
 
     // accept friend request (based on the FriendRequest model) -> essentially creating a new record in the friends table
     // also, has to delete the entire record on the friend_requests table using the id of the accepted user
-    ResponseEntity acceptFriend(Long requesterId, Long recipientId);
+    ResponseEntity acceptFriend(HashMap<String, Object> friendMap);
 
     // delete friend
     ResponseEntity deleteFriend(Long requesterId, Long recipientId);
+
+    // get all friends given a particular page id
+    ResponseEntity getFriends(Long pageId);
+
+    // get one friend
+    ResponseEntity getOneFriend(Long requesterId, Long recipientId);
 
 }
