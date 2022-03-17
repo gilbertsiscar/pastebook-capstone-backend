2aa416cfabdcffc679a88a5522d3d81d60f90728
Alexis Kyle Sales <alexis.sales@pointwest.com.ph>
2022-03-17 09:20:04 +0800

    Socket updates

--- src/main/java/com/pointwest/pastebook/pastebook_backend/sockets/SocketHandler.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/sockets/SocketHandler.java
@@ -9,13 +9,14 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
-
 @Component
 public class SocketHandler extends TextWebSocketHandler {
     List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+    Map<String, WebSocketSession> id_sessions = new HashMap<>();
     int messagecount = 0;
 
     @Override
@@ -25,11 +26,33 @@ public class SocketHandler extends TextWebSocketHandler {
         // parse message
         //Map<String, String> value = new Gson().fromJson(message.getPayload(), Map.class);
 
-         //send message to all sessions
-         for (WebSocketSession webSocketSession : sessions) {
-             Map value = new Gson().fromJson(message.getPayload(), Map.class);
-             webSocketSession.sendMessage(new TextMessage(message.getPayload()));
-         }
+        //send message to all sessions
+//         for (WebSocketSession webSocketSession : sessions) {
+//             //Map value = new Gson().fromJson(message.getPayload(), Map.class);
+//             webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+//             //webSocketSession.sendMessage(new TextMessage("Request:Update"));
+//         }
+
+        System.out.println("Message from client");
+        Map value = new Gson().fromJson(message.getPayload(), Map.class);
+        System.out.println(value.get("user_id"));
+        System.out.println(value.get("trigger"));
+        if(value.get("user_id") != null){
+            id_sessions.put(message.getPayload(),session);
+        }else{
+            //save database then trigger notif
+
+            for (Map.Entry<String, WebSocketSession> sendRequest : id_sessions.entrySet()) {
+                //Map value = new Gson().fromJson(message.getPayload(), Map.class);
+                //webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+
+                sendRequest.getValue().sendMessage(new TextMessage(message.getPayload()));
+            }
+
+        }
+        //id_sessions.put(message.getPayload(),session);
+
+
 
 //        if(value.keySet().contains("subscribe")) {
 //            // start the service with the subscribe name
@@ -56,6 +79,8 @@ public class SocketHandler extends TextWebSocketHandler {
         //System.out.println("Connection Establised");
         //System.out.println(session);
         //System.out.println(session.getId());
+        System.out.println(session.getId());
+        System.out.println(session.getAttributes());
         if(!sessions.contains(session)){
             sessions.add(session);
         }
@@ -63,7 +88,17 @@ public class SocketHandler extends TextWebSocketHandler {
 
         System.out.println(sessions.size());
     }
+    public void notifyUsers() throws IOException {
+        System.out.println("Connections: " + id_sessions.size() );
+        for (Map.Entry<String, WebSocketSession> sendRequest : id_sessions.entrySet()) {
+            //Map value = new Gson().fromJson(message.getPayload(), Map.class);
+            //webSocketSession.sendMessage(new TextMessage(message.getPayload()));
 
+            sendRequest.getValue().sendMessage(new TextMessage(
+                    (CharSequence) new Gson().fromJson("{'test':'pop'}}", Map.class)));
+            System.out.println("I was called");
+        }
+    }
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         // do something on connection closed
