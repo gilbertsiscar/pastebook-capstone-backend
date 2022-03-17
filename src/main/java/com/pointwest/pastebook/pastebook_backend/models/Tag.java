b11df528b90dcb199494baf79ee6d5b78e309bd3
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-17 21:14:11 +0800

    Created tag models

---
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/models/Tag.java
@@ -0,0 +1,54 @@
+package com.pointwest.pastebook.pastebook_backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tag")
+public class Tag {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  @JsonIgnore
+  private Post post;
+
+  public Tag(User user, Post post) {
+    this.user = user;
+    this.post = post;
+  }
+
+  public Tag() {}
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Post getPost() {
+    return post;
+  }
+
+  public void setPost(Post post) {
+    this.post = post;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
