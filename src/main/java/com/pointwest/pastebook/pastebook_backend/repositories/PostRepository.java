ae1c85e3a5c008f7fb1d3409c33b154e35d302e9
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-18 23:57:49 +0800

    Please

--- src/main/java/com/pointwest/pastebook/pastebook_backend/repositories/PostRepository.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.pointwest.pastebook.pastebook_backend.repositories;
 import com.pointwest.pastebook.pastebook_backend.models.Post;
 import com.pointwest.pastebook.pastebook_backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -10,5 +11,8 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+//    @Query(value = "SELECT * FROM POSTS WHERE `user.id` IN (SELECT  = ?1",
+//            countQuery = "SELECT count(*) FROM POSTS WHERE  = ?1",
+//            nativeQuery = true)
     List<Post> findByUser_IdIn(Collection<Long> id);
 }
