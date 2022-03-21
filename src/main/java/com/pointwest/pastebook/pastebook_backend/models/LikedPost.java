6171081512cee4484a58a5d1e80a8fac1f2d8862
gilbertsiscar <gilbert.siscar@pointwest.com.ph>
2022-03-21 14:38:03 +0800

    Fixed

--- src/main/java/com/pointwest/pastebook/pastebook_backend/models/LikedPost.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/models/LikedPost.java
@@ -12,16 +12,15 @@ public class LikedPost {
 
     // Properties
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "liked_posts_seq")
-    @SequenceGenerator(name = "liked_posts_seq", sequenceName = "sequence_liked_posts", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // This refers to the user who 'likes' the post
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "post_id")
     @JsonIgnore
     private Post post;
